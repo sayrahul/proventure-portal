2adbb2fbd963976c627170915a923cc431577984
Rahul Jadhav <sayrahul>
2026-02-18 11:51:38 +0530

    Fix hydration error and finalize project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.variable} ${outfit.variable} antialiased selection:bg-accent selection:text-white`}
       >
